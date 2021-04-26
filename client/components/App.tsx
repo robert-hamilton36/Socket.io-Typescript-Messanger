c4605a083056c68d54593434c72ac2f82afdf6cf
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-04-27 11:05:11 +1200

    add some context on behaviour

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,6 +3,9 @@ import useSocket from '../useSocket'
 import MessageInput from './MessageInput'
 import MessageInputNotForm from './MessageInputNotForm'
 
+//Werid behaviour:
+//Using <Form> causes odd behaviour to the customSocketHook
+
 const App: React.FC = () => {
   const [messages, sendMessage] = useSocket()
   const [newMessage, setNewMessage] = useState('')
