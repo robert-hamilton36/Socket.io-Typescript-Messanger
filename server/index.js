81fc39bfeb8938b9897afa85171af0e77db9d46b
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-04-27 16:19:18 +1200

    add socket-io

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const server = require('./server')
+const ioServer = require('./socket')
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
+const serverObj = server.listen(PORT, () => {
   console.log('Server listening on port', PORT)
 })
+
+ioServer(serverObj)
