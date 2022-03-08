599f77e846058549df669a86cae4ade59f0c7b5f
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-03-09 12:29:09 +1300

    remove old components

--- client/components/useUserContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-interface User{
-  name: string,
-  uid: number
-}
-
-// export const UserContext = createContext<User | null>(null)
-export const UserContext = createContext<User>({ name: '', uid: 0 })
--- client/testing/components/room/Room.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Room from '../../../components/room/Room'
-
-test('<Room> renders properly', () => {
-  const wrapper = shallow(<Room />)
-  expect(wrapper).toBeTruthy()
-})
